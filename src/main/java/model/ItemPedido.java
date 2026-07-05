8c1b5a29b2b586fd1e0c1f778073098ad386acf7
Rick <ricardo.nascimento.1@academico.ifpb.edu.br>
2026-07-05 09:39:35 -0300

    Finalmente o model deu certo

--- src/main/java/model/ItemPedido.java
+++ src/main/java/model/ItemPedido.java
@@ -1,4 +1,56 @@
 package model;
 
 public class ItemPedido {
+    private String itemId;
+    private String nome;
+    private double precoUnitario;
+    private int quantidade;
+
+    public ItemPedido(){
+
+    }
+    public ItemPedido(String itemId, String nome, double precoUnitario, int quantidade) {
+        this.itemId = itemId;
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getSubtotal() {
+        return precoUnitario * quantidade;
+    }
+
+    public void incrementar() {
+        quantidade++;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemPedido{" +
+                "itemId='" + itemId + '\'' +
+                ", nome='" + nome + '\'' +
+                ", precoUnitario=" + precoUnitario +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
--- src/main/java/model/Pedido.java
+++ src/main/java/model/Pedido.java
@@ -3,29 +3,72 @@ package model;
 import exception.CancelamentoNaoPermitidoException;
 import exception.StatusInvalidoException;
 
-import java.io.ObjectInputFilter;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+
 public class Pedido implements Identificador{
     private String id;
+    private String clienteEmail;
     private LocalDateTime data;
-    private List<Item> itens;
-    private ObjectInputFilter.Status status;
+    private List<ItemPedido> itens;
+    private double valorTotal;
+    private StatusPedido status = StatusPedido.Aguardando_Pedido;
 
-    public Pedido() {
-        this.id = UUID.randomUUID().toString();
-        this.data = LocalDateTime.now();
-        this.itens = new ArrayList<>();
-        this.status = ObjectInputFilter.Status.PENDENTE;
+    public Pedido(){
+
+    }
+    public Pedido(String clienteEmail, List<ItemPedido> itens) {
+        this.clienteEmail = clienteEmail;
+        this.itens = new ArrayList<>(itens);
+        recalcularTotal();
     }
 
-    @Override
     public String getId() {
         return id;
     }
 
+    public String getClienteEmail() {
+        return clienteEmail;
+    }
 
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+    public void avancarStatus() throws StatusInvalidoException {
+        status = status.proximo();
+    }
+    public void cancelar() throws CancelamentoNaoPermitidoException {
+        if (status != StatusPedido.Pronto) {
+            throw new CancelamentoNaoPermitidoException("Pedido só pode ser cancelado enquanto aguarda confirmação.");
+        }
+        status = StatusPedido.Cancelado;
+    }
+    public boolean estaAberto() {
+        return status != StatusPedido.Entregue && status != StatusPedido.Cancelado;
+    }
+    private void recalcularTotal() {
+        valorTotal = itens.stream().mapToDouble(ItemPedido::getSubtotal).sum();
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id='" + id.substring(0, 8) + '\'' +
+                ", emailCliente='" + clienteEmail + '\'' +
+                ", data=" + data +
+                ", itens=" + itens +
+                ", status=" + status +
+                '}';
+    }
 }
